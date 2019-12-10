521149676ff6b7df95ecd448ca66fdcb7249d228
rim-code <friouirim@gmail.com>
2019-12-10 11:19:45 +0100

    Updates

---
+++ precache-manifest.33febb6ecf29b10cb45fc0bb3954c64e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d27f09cb501c1a5139e8a8581639cef",
+    "url": "/movieapp/index.html"
+  },
+  {
+    "revision": "af91e55841a1cfa74714",
+    "url": "/movieapp/static/css/main.da76d6a6.chunk.css"
+  },
+  {
+    "revision": "6b6a3c9ddec2d7eafedd",
+    "url": "/movieapp/static/js/2.50e28f87.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movieapp/static/js/2.50e28f87.chunk.js.LICENSE"
+  },
+  {
+    "revision": "af91e55841a1cfa74714",
+    "url": "/movieapp/static/js/main.c256c652.chunk.js"
+  },
+  {
+    "revision": "4a21fb05c7417c222c9e",
+    "url": "/movieapp/static/js/runtime-main.3f413910.js"
+  }
+]);
